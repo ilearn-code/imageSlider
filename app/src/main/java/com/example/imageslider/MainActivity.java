397c1ef0297b713f546fb82afea309ab35fdf382
ilearn-code <satyamyadavv931@gmail.com>
2023-03-30 16:28:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.imageslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    int[] images = {R.drawable.brochure,R.drawable.namecard};
+    adapter mViewPagerAdapter;
+    ViewPager mViewPager;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager)findViewById(R.id.viewPagerMain);
+
+        // Initializing the ViewPagerAdapter
+        mViewPagerAdapter = new adapter(MainActivity.this, images);
+
+        // Adding the Adapter to the ViewPager
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+    }
+}
